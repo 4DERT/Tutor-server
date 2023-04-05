6accfbcca3223e09de6a0ddd36a95d734d5c6189
4DERT <4dertdk@gmail.com>
2023-04-05 23:09:20 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# db.create_all()
+
