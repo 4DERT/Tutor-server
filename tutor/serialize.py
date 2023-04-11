778713cce44b6e8ef8c6520332f6b61b53a33473
Pawel21-37 <169527@stud.prz.edu.pl>
2023-04-11 23:58:23 +0200

    Add announcement filtering and changed date format to YYYY-mm-dd

--- tutor/serialize.py
+++ tutor/serialize.py
@@ -1,12 +1,43 @@
 from .models import Announcement, Location, Subject, User
+from . import db
 
 
-def get_announcements():
+def get_announcements(price_from: int | None,
+                      price_to: int | None,
+                      location: str | None,
+                      subject: str | None,
+                      is_negotiable: bool | None,
+                      date_posted_from: str | None,
+                      date_posted_to: str | None):
+
+    subject_id = Subject.query.filter_by(subject=subject).first().id if subject else None
+    location_id = Location.query.filter_by(location=location).first().id if location else None
+
+    # filter_by
+    filters_by = {
+        "location_id": location_id,
+        "subject_id": subject_id,
+        "is_negotiable": is_negotiable,
+    }
+    final_filters_by = {key: val for key, val in filters_by.items() if val is not None}
+
+    # filter
+    filters = [
+        (Announcement.price >= price_from) if price_from else None,
+        (Announcement.price <= price_to) if price_to else None,
+        (Announcement.date_posted >= date_posted_from) if date_posted_from else None,
+        (Announcement.date_posted <= date_posted_to) if date_posted_to else None
+    ]
+
+    final_filters = [f for f in filters if f is not None]
+
+    query = Announcement.query.filter_by(**final_filters_by).filter(*final_filters)
+
     return [
         {
             'id': obj.id,
             'title': obj.title,
-            'date_posted': obj.date_posted,
+            'date_posted': obj.date_posted.strftime("%Y-%m-%d"),
             'content': obj.content,
             'price': obj.price,
             'is_negotiable': obj.is_negotiable,
@@ -17,7 +48,7 @@ def get_announcements():
             'announcer_phone': obj.author.phone,
             'subject': obj.subject.subject,
             'location': obj.location.location
-        } for obj in Announcement.query.all()
+        } for obj in query
     ]
 
 
